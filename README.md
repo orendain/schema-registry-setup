10a79aad4e5acd3c123cf03fe3e45334ac4e9172
Edgar Orendain <edgar@orendainx.com>
2017-02-10 10:58:36 -0800

    Cleaned up script for both HDP/HDF use

--- README.md
+++ README.md
@@ -1,14 +1,13 @@
 # Schema Registry Setup
 
-This installs and configures Schema Registry onto your latest HDF Sandbox.  It also installs a NiFi nar for NiFi integration.
+This installs and configures Schema Registry onto the latest HDF/HDP Sandbox.  It also installs a Schema Registry NiFi nar for NiFi integration.
 
 Run the following in the sandbox:
 ```
 git clone https://github.com/orendain/schema-registry-setup
-./schema-registry-setup/setup-registry.sh
+schema-registry-setup/setup-registry.sh
 ```
 
-That's it!
+That's it! :)
 
-Note: Since Schema Registry is going to be released in the very near future, this script was written with speed in mind, not necessarily flexibility or to be future-proof.  Lots of hard coded goodness going on in here ;)
-
+Note: Private repo until further notice.
--- setup-registry.sh
+++ setup-registry.sh
@@ -1,32 +1,39 @@
 #!/bin/bash
 
 # Variables
-PROJ_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+schemaVer="0.0.1.3.0.0.0-9"
+projDir="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+
+registryArr=($(find / -type f -name "registry"))
+registry=${registryArr[-1]}
+registryUserName="registry_user"
+registryPass="R12034ore"
+
+platformDir=$(dirname $(find / -path "*/kafka/bin"))
+nifiLibs=($(find / -path "/usr/*/nifi/lib"))
 
 # Move to tmp directory
 cd /tmp
 
-# Download Schema Registry binaries
-wget http://nexus-private.hortonworks.com/nexus/content/groups/public/com/hortonworks/registries/hortonworks-registries-bin/0.0.1.2.2.0.0-19/hortonworks-registries-bin-0.0.1.2.2.0.0-19.tar.gz
+# Download binaries and extract to proper location
+wget http://nexus-private.hortonworks.com/nexus/content/groups/public/com/hortonworks/registries/hortonworks-registries-bin/${schemaVer}/hortonworks-registries-bin-${schemaVer}.tar.gz
+tar zxvf hortonworks-registries-bin-${schemaVer}.tar.gz
+mv hortonworks-registry-${schemaVer} $platformDir/registry
 
-tar zxvf hortonworks-registries-bin-0.0.1.2.2.0.0-19.tar.gz
-mv hortonworks-registry-0.0.1.2.2.0.0-19/ /usr/hdf/2.1.0.0-165/registry
+# Create symlink in current directory
+ln -s $platformDir/registry $(dirname $platformDir)/current
 
-ln -s /usr/hdf/2.1.0.0-165/registry /usr/hdf/current/registry
+# (Sandbox only, due to default username/pass)
+echo "create database schema_registry; CREATE USER '$registryUserName'@'localhost' IDENTIFIED BY '$registryPass'; GRANT ALL PRIVILEGES ON schema_registry.* TO '$registryUserName'@'localhost' WITH GRANT OPTION;" > tmpQuery.sql
+mysql -u root -phadoop < tmpQuery.sql
 
+# Replace configs with appropriate values
+perl -pi -e 's/9090/8090/g' $platformDir/registry/conf/registry.yaml
+perl -pi -e 's/registry_password/$registryPass/g' $platformDir/registry/conf/registry.yaml
 
-echo "create database schema_registry; CREATE USER 'registry_user'@'localhost' IDENTIFIED BY 'R12034ore'; GRANT ALL PRIVILEGES ON schema_registry.* TO 'registry_user'@'localhost' WITH GRANT OPTION;" > tmpp.sql
-mysql -u root -phadoop < tmpp.sql
-
-
-perl -pi -e 's/9090/8090/g' /usr/hdf/current/registry/conf/registry.yaml
-perl -pi -e 's/registry_password/R12034ore/g' /usr/hdf/current/registry/conf/registry.yaml
-
-
-/usr/hdf/current/registry/bootstrap/bootstrap-storage.sh
-
-# Start in daemon mode
-/usr/hdf/current/registry/bin/registry start
+# Bootstrap storage and follow-up with starting in daemon mode
+$platformDir/registry/bootstrap/bootstrap-storage.sh
+$platformDir/registry/bin/registry start
 
 # Install Schema Registry nar file
-cp $PROJ_DIR/nifi-registry-nar-0.0.1-SNAPSHOT.nar /usr/hdf/current/nifi/lib
+cp $projDir/nifi-registry-nar-0.0.1-SNAPSHOT.nar ${nifiLibs[0]}
